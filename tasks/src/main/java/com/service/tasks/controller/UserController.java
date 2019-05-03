4573cddcf0e50f8b16d40c170c5417af8e5db36f
mccarrion <mccarrion@users.noreply.github.com>
2019-05-02 17:17:05 -0700

    Setup model and controller folders with initial User code

---
+++ tasks/src/main/java/com/service/tasks/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.service.tasks.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User API
+ */
+@RestController
+public class UserController {
+    
+}
