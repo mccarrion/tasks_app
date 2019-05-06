15d172218a66af07e9a0831a0050ab99e446eef1
mccarrion <mccarrion@users.noreply.github.com>
2019-05-05 19:50:38 -0700

    Postgres config, new dependency, and folder name change

--- tasks/src/main/java/com/service/tasks/controllers/UserController.java
+++ tasks/src/main/java/com/service/tasks/controllers/UserController.java
@@ -1,11 +1,13 @@
-package com.service.tasks.controller;
+package com.service.tasks.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * User API
  */
 @RestController
+@RequestMapping("/users/")
 public class UserController {
     
 }
