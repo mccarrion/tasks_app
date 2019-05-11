2e67676d23fe1f0fff7dff505a0ffe02acdf7daa
mccarrion <mccarrion@users.noreply.github.com>
2019-05-10 20:54:07 -0700

    Setting up Repository for User API endpoint

--- tasks/src/main/java/com/service/tasks/controllers/UserController.java
+++ tasks/src/main/java/com/service/tasks/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.service.tasks.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
  * User API
  */
 @RestController
-@RequestMapping("/users/")
+@RequestMapping(value = "/users/")
 public class UserController {
     
+    @Autowired
+    UserRepository userRepository;
+
 }
