4852dd15160707ede0e8e75665fa9af8f734879a
mccarrion <mccarrion@users.noreply.github.com>
2019-05-14 07:38:43 -0700

    Finished up initial user model and correct type on repository

--- tasks/src/main/java/com/service/tasks/models/User.java
+++ tasks/src/main/java/com/service/tasks/models/User.java
@@ -37,14 +37,12 @@ public class User extends Base {
         this.password = password;
     }
 
-    @JsonIgnore
-    public String getPassword() {
-        return password;
+    public String getUsername() {
+        return username;
     }
 
-    @JsonProperty
-    public void setPassword(String password) {
-        this.password = password;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
@@ -53,6 +51,16 @@ public class User extends Base {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Collection<Task> getTasks() {
