93ce80e866039ce453402de564eb2046cdd7edc3
mccarrion <mccarrion@users.noreply.github.com>
2019-05-20 20:46:01 -0700

    Put together draft of securing endpoints with JWT

--- tasks/src/main/java/com/service/tasks/services/UserService.java
+++ tasks/src/main/java/com/service/tasks/services/UserService.java
@@ -1,30 +1,32 @@
 package com.service.tasks.services;
 
-import javax.transaction.Transactional;
-
+import com.service.tasks.models.User;
 import com.service.tasks.repositories.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-@Transactional
-public class UserService implements UserDetailsService {
+import java.util.Collections;
 
-    @Autowired
+@Service
+public class UserService implements UserDetailsService {
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder PasswordEncoder;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
     }
-
-    
 }
