d16a247017ea9e6cbfd0fae08d318c5e477d69e3
Jude Brauer <jxxb0000@gmail.com>
2019-06-16 18:40:23 -0700

    Work on tasks UI

--- client/components/ListItem.js
+++ client/components/ListItem.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import CheckBox from 'react-native-check-box';
 
 export class ListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {checked: this.props.data.checked};
+    }
+
+    onCheckedChange = () => {
+        this.setState({checked: !this.state.checked}, () => {
+            this.props.onCheckedChange(this.state.checked);
+        });
+    }
+
     render() {
         return (
-            <View>
-                <Text>{this.props.data.title}</Text>
-                <Text>{this.props.data.description}</Text>
+            <View style={styles.listItem}>
+                <CheckBox rightText={this.props.data.title} checkBoxColor="#2F95DC" isChecked={this.state.checked} onClick={this.onCheckedChange}/>
             </View>
-            );
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    listItem: {
+        paddingLeft: 15,
+        paddingTop: 15,
+    },
+});
--- client/screens/TasksScreen.js
+++ client/screens/TasksScreen.js
@@ -5,20 +5,24 @@ import {
     Text,
     View,
     FlatList,
+    Modal,
+    TextInput,
+    Button,
+    TouchableOpacity,
 } from 'react-native';
 
 import ActionButton from 'react-native-action-button';
-import Dialog from 'react-native-dialog';
 
 import {ListItem} from '../components/ListItem.js';
 
 export default class TasksScreen extends React.Component {
     static navigationOptions = {
-        header: null,
+        title: "Tasks",
     };
 
     constructor(props) {
         super(props);
+
         this.state = {
             tasks: [],
             showAddDialog: false,
@@ -28,38 +32,64 @@ export default class TasksScreen extends React.Component {
     }
 
     onAddClicked = () => {
-        console.log("Testing");
-        this.setState({ showAddDialog: true });
+        this.setState({
+            showAddDialog: true,
+            newTaskTitle: "",
+            newTaskDescription: "",
+        });
     }
 
     addTask = () => {
         this.setState({
+            tasks: [...this.state.tasks, {title: this.state.newTaskTitle, description: this.state.newTaskDescription, checked: false}],
             showAddDialog: false,
-            tasks: [...this.state.tasks, {title: this.state.newTaskTitle, description: this.state.newTaskDescription}],
-        })
+        });
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <FlatList
-                    style={styles.contentContainer}
                     data={this.state.tasks}
-                    renderItem={({item}) => <ListItem data={item}/>}/>
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item}) => <ListItem data={item} onCheckedChange={(checked) => {item.checked = checked}}/>}/>
 
                 <ActionButton
                     buttonColor="#2F95DC"
                     onPress={this.onAddClicked}/>
 
-                <Dialog.Container visible={this.state.showAddDialog}>
-                    <Dialog.Title>Add new task</Dialog.Title>
-                    <Dialog.Input label="Title" onChangeText={(newTaskTitle) => this.setState({newTaskTitle})}/>
-                    <Dialog.Input label="Description" onChangeText={(newTaskDescription) => this.setState({newTaskDescription})}/>
-                    <Dialog.Button label="Cancel" onPress={() => {this.setState({ showAddDialog: false })}}/>
-                    <Dialog.Button label="Add" onPress={this.addTask}/>
-                </Dialog.Container>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.showAddDialog}
+                    onRequestClose={() => this.setState({showAddDialog: false})}>
+
+                    <View style={styles.modalContainer}>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={() => {this.setState({showAddDialog: false})}}>
+                                <Text style={styles.taskModalButton}>Cancel</Text>
+                            </TouchableOpacity>
+
+                            <Text style={styles.newTaskHeader}>Add new task</Text>
+
+                            <TouchableOpacity onPress={this.addTask}>
+                                <Text style={styles.taskModalButton}>Finish</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <TextInput style={styles.textInput} placeholder="Title" value={this.state.newTaskTitle} onChangeText={(newTaskTitle) => this.setState({newTaskTitle})}/>
+                        <TextInput style={styles.textInput} placeholder="Description" value={this.state.newTaskDescription} onChangeText={(newTaskDescription) => this.setState({newTaskDescription})}/>
+
+                        {/*
+                        <View style={styles.row}>
+                        <Button style={styles.button} title="Cancel" onPress={() => {this.setState({showAddDialog: false})}}/>
+                        <Button style={styles.button} title="Add" onPress={this.addTask}/>
+                        </View>
+                        */}
+                    </View>
+                </Modal>
             </View>
-            );
+        );
     }
 }
 
@@ -68,86 +98,21 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
+    modalContainer: {
+        margin: 15,
     },
-    contentContainer: {
-        paddingTop: 30,
+    newTaskHeader: {
+        fontSize: 24,
     },
-    welcomeContainer: {
-        alignItems: 'center',
+    textInput: {
         marginTop: 10,
-        marginBottom: 20,
     },
-    welcomeImage: {
-        width: 100,
-        height: 80,
-        resizeMode: 'contain',
-        marginTop: 3,
-        marginLeft: -10,
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
-    getStartedContainer: {
-        alignItems: 'center',
-        marginHorizontal: 50,
-    },
-    homeScreenFilename: {
-        marginVertical: 7,
-    },
-    codeHighlightText: {
-        color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 3,
-        paddingHorizontal: 4,
-    },
-    getStartedText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 24,
-        textAlign: 'center',
-    },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: { height: -3 },
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
-    tabBarInfoText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        textAlign: 'center',
-    },
-    navigationFilename: {
-        marginTop: 5,
-    },
-    helpContainer: {
-        marginTop: 15,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
+    taskModalButton: {
+        fontSize: 24,
+        color: "#2F95DC",
     },
 });
