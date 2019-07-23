3750fcd4e54936729009fdfc293d2333c0cef7b6
mccarrion <mccarrion@users.noreply.github.com>
2019-07-22 20:31:58 -0700

    Retrieving AUTH_TOKEN correctly now

--- client/constants/General.js
+++ client/constants/General.js
@@ -1,8 +1,14 @@
 import { AsyncStorage } from 'react-native';
 
-// Need to get token from local storage to add it for token with Auth prefix
-const userToken = AsyncStorage.getItem('userToken');
-
 // Global Constants
 export const API_URL = 'http://localhost:8080';
-export const AUTH_TOKEN = `Bearer ${userToken}`;
+
+// Need to get token from local storage
+export const AUTH_TOKEN = async () => {
+  try {
+    const retrievedToken = await AsyncStorage.getItem("userToken");
+    return retrievedToken;
+  } catch(error) {
+    alert("Token retrieval failed");
+  }
+}
