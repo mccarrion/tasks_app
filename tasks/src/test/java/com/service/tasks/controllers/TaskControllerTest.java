4b469873054c86c9ec56efcb3947fed8258aaf6d
mccarrion <mccarrion@users.noreply.github.com>
2019-07-31 18:15:43 -0700

    Updated package-lock.json and further debugging of Unit tests

--- tasks/src/test/java/com/service/tasks/controllers/TaskControllerTest.java
+++ tasks/src/test/java/com/service/tasks/controllers/TaskControllerTest.java
@@ -4,8 +4,11 @@ import com.service.tasks.models.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,6 +37,7 @@ public class TaskControllerTest {
     }
 
     @Test
+    @WithMockUser(username="john")
     public void getTasksByUser() throws Exception {
         /**
          * TODO: Uncomment this and integrate into test
