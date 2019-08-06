2c6b34d8cbf813cc16982b47cb65494d40edb40a
mccarrion <mccarrion@users.noreply.github.com>
2019-08-06 10:17:41 -0700

    AUTH_TOKEN removed, token now being retrieved in methods

--- client/constants/General.js
+++ client/constants/General.js
@@ -1,14 +1,2 @@
-import { AsyncStorage } from 'react-native';
-
 // Global Constants
 export const API_URL = 'http://localhost:8080';
-
-// Need to get token from local storage
-export const AUTH_TOKEN = async () => {
-  try {
-    const retrievedToken = await AsyncStorage.getItem("userToken");
-    return retrievedToken;
-  } catch(error) {
-    alert("Token retrieval failed");
-  }
-}
