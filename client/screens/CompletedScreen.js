5a540f299e5b7ea93b282c69a69c4c707752fa01
mccarrion <mccarrion@users.noreply.github.com>
2019-08-08 20:12:15 -0700

    CompletedScreen now working

--- client/screens/CompletedScreen.js
+++ client/screens/CompletedScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {
-  StyleSheet,
-  View,
+  AsyncStorage,
   FlatList,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from 'react-native';
+
 import { TaskItem } from '../components/TaskItem';
 import { API_URL } from '../constants/General';
 
@@ -79,6 +85,34 @@ export default class CompletedScreen extends React.Component {
     this.setState({showEditWindow: false});
   }
 
+  onFinishClicked = () => {
+    let tasks = [...this.state.tasks];
+
+    if (this.state.currentTaskTitle !== "") {
+      if (this.state.newTask) {
+        this.createTask(this.state.currentTaskTitle, this.state.currentTaskContent).then((newTask) => {
+          tasks.push(newTask);
+          this.setState({tasks});
+        });
+      } else {
+        let currentTask = tasks[this.state.currentTaskIndex];
+        currentTask.title = this.state.currentTaskTitle;
+        currentTask.content = this.state.currentTaskContent;
+
+        this.setState({tasks});
+        this.updateTask(currentTask);
+      }
+    } else if (!this.state.newTask) {
+      const currentTask = tasks[this.state.currentTaskIndex];
+      tasks.splice(this.state.currentTaskIndex, 1);
+
+      this.setState({tasks});
+      this.deleteTask(currentTask);
+    }
+
+    this.setState({showEditWindow: false});
+  }
+
   onTaskCheckedChange = (index) => {
     let tasks = [...this.state.tasks];
     let currentTask = tasks[index];
@@ -94,16 +128,50 @@ export default class CompletedScreen extends React.Component {
       <View style={styles.container}>
         <FlatList
           data={this.state.tasks}
-          keyExtractor={(item, index) => index}
-          renderItem={({item}) => (
-            <Text>{item.title}</Text>
-            // <TaskItem
-            //   data={item}
-            //   onClick={() => this.onItemClicked(index)}
-            //   onTaskCheckedChange={() => this.onTaskCheckedChange(index)}
-            // />
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => (
+            <TaskItem
+              task={item}
+              onClick={() => this.onItemClicked(index)}
+              onCheckedChange={() => this.onTaskCheckedChange(index)}
+            />
           )}
         />
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.showEditWindow}
+          onRequestClose={() => this.setState({showEditWindow: false})}>
+
+          <View style={styles.modalContainer}>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={this.onCancelClicked}>
+                <Text style={styles.taskModalButton}>Cancel</Text>
+              </TouchableOpacity>
+
+              <Text style={styles.currentTaskHeader}>
+                {this.state.newTask ? "New Task" : "Edit Task"}
+              </Text>
+
+              <TouchableOpacity onPress={this.onFinishClicked}>
+                <Text style={styles.taskModalButton}>Finish</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder="Title"
+              value={this.state.currentTaskTitle}
+              onChangeText={this.onTaskTitleChange}/>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder="Content"
+              value={this.state.currentTaskContent}
+              onChangeText={this.onTaskContentChange}/>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -112,11 +180,23 @@ export default class CompletedScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  modalContainer: {
+    margin: 15,
+  },
+  currentTaskHeader: {
+    fontSize: 24,
+  },
+  textInput: {
+    marginTop: 10,
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  taskModalButton: {
+    fontSize: 24,
+    color: "#2F95DC",
   },
 });
