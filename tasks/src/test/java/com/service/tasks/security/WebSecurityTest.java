73195e75911182f35c3f87da723a5e5543b497b9
mccarrion <mccarrion@users.noreply.github.com>
2019-08-10 18:39:16 -0700

    Setup basic security config for Spring unit tests

--- tasks/src/test/java/com/service/tasks/security/WebSecurityTest.java
+++ tasks/src/test/java/com/service/tasks/security/WebSecurityTest.java
@@ -1,12 +1,37 @@
 package com.service.tasks.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * This is an experimental test config for Spring Security
+ */
 @Configuration
 @EnableWebSecurity
 @ActiveProfiles("test")
 public class WebSecurityTest extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser("John")
+                .password(encoder.encode("password"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**")
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
